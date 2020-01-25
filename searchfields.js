1e1f843c91a812adce6f25b498db75bce0d940ab
flanneryc <flannery.carlos@gmail.com>
2020-01-25 08:23:38 -0800

    added a clear button to clear the fields, dont murder me bc it needs to be aligned in the row

--- searchfields.js
+++ searchfields.js
@@ -16,9 +16,16 @@ function runQuery(queryURL){
 
     //AJAX Function
     $.ajax({url: queryURL, method: "GET"})
-    .done(function(OLData) {
-        console.log(queryURL);
-        console.log(OLData);
+        .done(function(OLData) {
+            for (let i=0; i<OLData.response.docs.length; i++){
+                console.log(OLData.response.docs[i].title_suggest);
+                console.log(OLData.response.docs[i].author_name[0]);
+                console.log(OLData.response.docs[i].first_publish_year);
+                console.log(OLData.response.docs[i].subject);
+            }
+   
+        // console.log(queryURL);
+        // console.log(OLData);
     })
 }
 
@@ -28,18 +35,50 @@ function runQuery(queryURL){
 
 $('#searchbtn').on('click', function(){
 
+// // 1. Retrieve user input and convert into variables
+
     let searchTitle = $('#booktitle').val().trim();
-        console.log(searchTitle);
+    console.log(searchTitle);
+
+    let searchAuthor = $('#bookauthor').val().trim();
+    console.log(searchAuthor);
+
+    let searchKeyword = $('#bookkeyword').val().trim();
+    console.log(searchKeyword);
+
+    let searchSubject = $('#booksubject').val().trim();
+    console.log(searchSubject);
+
     
-    let newURL = urlBase + "&title=" + searchTitle;
-        console.log(newURL);
-        
-        runQuery(newURL);
+    // let queryURL = '';
+
+    // if (searchTitle !== null) {
+    let queryURL = urlBase + "&title=" + searchTitle;
+    console.log(queryURL);
+    // }
+
+//     if (searchAuthor !== null){
+//         let queryURL = urlBase + "&a=" + searchAuthor;
+//         console.log(newURL);
+//     }
+
+//     if (searchKeyword !== null){
+//         let queryURL = urlBase + "&q=" + searchKeyword;
+//         console.log(newURL);
+//     }
+
+//     if (searchSubject !== null){
+//         let queryURL = urlBase + "&s=" + searchSubject;
+//         console.log(newURL);
+//     }
+
+// 2. Use those variables to run an AJAX call to Open Library
+    runQuery(queryURL);
+
 })
 
-// 1. Retrieve user input and convert into variables
-// 2. Use those variables to run an AJAX call to Open Library
-// 3. Break down the Open Library Object into useable fields
-// 4. Dynamically generate html content
 
-// 5. Dealing with edge cases (bugs or situations)
+// // 3. Break down the Open Library Object into useable fields
+// // 4. Dynamically generate html content
+
+// // 5. Dealing with edge cases (bugs or situations)
