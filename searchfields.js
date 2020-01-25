9101135a854611a9a6cc4d633f586e0a0d0bfd07
flanneryc <flannery.carlos@gmail.com>
2020-01-24 22:53:37 -0800

    added jquery and js links, swapped up search fields order, added 'or' before subject, added 's' to local bookstores nav bar link, added beginning of searchfields.js, added ids to html search divs

--- searchfields.js
+++ searchfields.js
@@ -1,3 +1,45 @@
-<script>
-    just checking
-</script>
+// SETUP VARIABLES
+// =======================================================
+let title   = "";
+let author  = "";
+let keyword = "";
+let subject = "";
+
+// URL Base for API Call
+let urlBase = "http://openlibrary.org/search.json?";
+
+
+// FUNCTIONS
+// =======================================================
+
+function runQuery(queryURL){
+
+    //AJAX Function
+    $.ajax({url: queryURL, method: "GET"})
+    .done(function(OLData) {
+        console.log(queryURL);
+        console.log(OLData);
+    })
+}
+
+
+// MAIN PROCESSES (FUNCTION CALLS)
+// =======================================================
+
+$('#searchbtn').on('click', function(){
+
+    let searchTitle = $('#booktitle').val().trim();
+        console.log(searchTitle);
+    
+    let newURL = urlBase + "&title=" + searchTitle;
+        console.log(newURL);
+        
+        runQuery(newURL);
+})
+
+// 1. Retrieve user input and convert into variables
+// 2. Use those variables to run an AJAX call to Open Library
+// 3. Break down the Open Library Object into useable fields
+// 4. Dynamically generate html content
+
+// 5. Dealing with edge cases (bugs or situations)
